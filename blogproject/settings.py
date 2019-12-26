a28e19b014d378f68a89909b78c9bbd04ac38b1a
lssx <196930850@qq.com>
2019-12-26 20:47:59 +0800

    添加了django-pure-pagination 配置

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'comments.apps.CommentsConfig',
     'pure_pagination',
 ]
+PAGINATION_SETTINGS = {
+    'PAGE_RANGE_DISPLAYED': 4,  # 分页条当前页前后应该显示的总页数（两边均匀分布，因此要设置为偶数），
+    'MARGIN_PAGES_DISPLAYED': 2,  # 分页条开头和结尾显示的页数
+    'SHOW_FIRST_PAGE_WHEN_INVALID': True,  # 当请求了不存在页，显示第一页
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
